34350e998c2ab102096c59219b2878a2f8d11998
Nisha Hitesh Rathod <109743699+nisharathod2301@users.noreply.github.com>
2022-08-07 12:21:42 +0530

    BackTracking

---
+++ nQueens.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        columns = set() 
+        positive_diagonal = set()
+        negative_diagonal = set()
+        
+        result=[]
+        board=[["."] * n for i in range(n)]
+        
+        def backtracking(r):
+          if r==n:
+            copy = ["".join(row) for row in board]
+            result.append(copy)
+            return
+          for c in range(n):
+            if c in columns or (r+c) in positive_diagonal or (r-c) in negative_diagonal:
+              continue
+            columns.add(c)
+            positive_diagonal.add(r+c)
+            negative_diagonal.add(r-c)
+            board[r][c]="Q"
+            
+            backtracking(r+1)
+            
+            columns.remove(c)
+            positive_diagonal.remove(r+c)
+            negative_diagonal.remove(r-c)
+            board[r][c]="."
+            
+        backtracking(0)
+        return result
